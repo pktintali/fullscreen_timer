b9c286fc9dfe4ac0845df913867d2f98d08cca9d
pradeepkumar8_deloitte <pradeepkumar8@deloitte.com>
2024-07-21 15:05:39 +0530

    first commit

---
+++ lib/widgets/timer_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TimerWidget extends StatelessWidget {
+  const TimerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Timer Widget'),
+      ),
+    );
+  }
+}
