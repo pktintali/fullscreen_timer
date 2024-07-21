7ca0b94b767f472ae9c85ac556c58e77cf424107
pradeepkumar8_deloitte <pradeepkumar8@deloitte.com>
2024-07-21 22:10:32 +0530

    timer widget design

--- lib/widgets/timer_widget.dart
+++ lib/widgets/timer_widget.dart
@@ -5,9 +5,56 @@ class TimerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Timer Widget'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              height: 20,
+              color: Colors.white38,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              color: Colors.white38,
+              width: 20,
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Container(
+              color: Colors.white38,
+              width: 20,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 20,
+              color: Colors.white38,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(20),
+            color: Colors.black,
+            child: const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    '00:00:00',
+                    style: TextStyle(
+                      fontSize: 60,
+                      color: Colors.white,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
